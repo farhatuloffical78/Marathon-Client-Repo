70f27e86ebf837f1eb06376f57ab8059481fe3aa
farhatuloffical78 <mdfarhatulhaider@gmail.com>
2025-02-12 15:33:16 +0600

    third commit

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -6,6 +6,12 @@ const About = () => {
             <header className="text-center mb-8">
                 <h1 className="text-4xl font-extrabold text-gray-800">About <span className="text-blue-600">RaceFlow</span></h1>
             </header>
+
+            <section className="mb-8 text-gray-700">
+                <p className="text-lg leading-relaxed">
+                    Welcome to <span className="font-semibold text-blue-600">RaceFlow</span>, your ultimate companion for marathons and running events! Whether you're a seasoned runner or just starting out, we provide everything you need to plan, track, and improve your marathon journey.
+                </p>
+            </section>
         </div>
     );
 };
